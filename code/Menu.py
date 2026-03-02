cdf8f795dc3b71cfadd312a1882b65e50f28be2d
Matheus Amon dos Santos Ferreira <amonmatheus757@gmail.com>
2026-03-02 12:25:40 -0300

    Revert "feat: add initial classes Game and Menu"
    This reverts commit 434b107b1b6cef24e87b1603f317e365ea17c71b.

--- code/Menu.py
+++
@@ -1,9 +0,0 @@
-from pygame.surface import SurfaceType
-
-
-class Menu:
-    def __init__(self, window: SurfaceType):
-        self.window = window
-
-    def run(self):
-        pass
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from code.Game import Game
+import pygame as app
 
-game = Game()
-game.run()
+app.init()
+
+screen = app.display.set_mode(size=(1080, 640))
+
+
+while True:
+    # check for all events
+    for event in app.event.get():
+        if event.type == app.QUIT:
+            app.quit() # close window
+            quit()  # close pygame
