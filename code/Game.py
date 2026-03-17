ee8ce9f1ed34dd8e26c2415db50389bc8e92be39
Matheus Amon dos Santos Ferreira <amonmatheus757@gmail.com>
2026-03-17 14:27:19 -0300

    feat: add level 2

--- code/Game.py
+++ code/Game.py
@@ -16,8 +16,13 @@ class Game:
             menu_response = menu.run()
 
             if menu_response in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
-                level = Level(self.window, "Level1", menu_response)
-                level_response = level.run()
+                player_score = [0, 0]
+                level = Level(self.window, "Level1", menu_response, player_score)
+                level_response = level.run(player_score)
+
+                if level_response:
+                    level_2 = Level(self.window, "Level2", level_response, player_score)
+                    level_2.run(player_score)
             elif menu_response == MENU_OPTION[3]:
                 pass
             else:
